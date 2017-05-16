b9e7e32f54838a609beb60bf5cdc5bd34bb57ebb
Halil COŞDU <halilcosdu@gmail.com>
2017-05-16 14:33:16 +0300

    env removed

--- routes/backend/backend.php
+++ routes/backend/backend.php
@@ -6,7 +6,6 @@
  * Time: 11:51
  */
 
-
 Route::group(['middleware' => 'can:' . env('PANEL-YÖNETİMİ') . ''], function () {
     Route::get('dashboard', 'Backend\DashboardController')->name('backend.dashboard');
     Route::group(['middleware' => 'can:' . env('SİSTEM-YÖNETİMİ') . ''], function () {
@@ -24,13 +23,10 @@ Route::group(['middleware' => 'can:' . env('PANEL-YÖNETİMİ') . ''], function 
     Route::group(['middleware' => 'can:' . env('DAVA-YÖNETİMİ') . ''], function () {
         Route::get('trials/{trial}/status', 'Backend\TrialController@status')->name('trials.status');
         Route::post('trials/{trial}/status', 'Backend\TrialController@statusStore')->name('trials.statusStore');
-
         Route::get('trials/{trial}/attorney', 'Backend\TrialController@attorney')->name('trials.attorney');
         Route::post('trials/{trial}/attorney', 'Backend\TrialController@attorneyStore')->name('trials.attorneyStore');
-
         Route::get('trials/{trial}/client', 'Backend\TrialController@client')->name('trials.client');
         Route::post('trials/{trial}/client', 'Backend\TrialController@clientStore')->name('trials.clientStore');
-
         Route::resource('trials', 'Backend\TrialController');
         Route::resource('statuses', 'Backend\StatusController');
     });
@@ -38,7 +34,6 @@ Route::group(['middleware' => 'can:' . env('PANEL-YÖNETİMİ') . ''], function 
     Route::group(['middleware' => 'can:' . env('MÜVEKKİL-YÖNETİMİ') . ''], function () {
         Route::resource('clients', 'Backend\ClientController');
     });
-
 
     Route::group(['middleware' => 'can:' . env('İÇERİK-YÖNETİMİ') . ''], function () {
         Route::resource('announcements', 'Backend\AnnouncementController');
@@ -52,7 +47,6 @@ Route::group(['middleware' => 'can:' . env('PANEL-YÖNETİMİ') . ''], function 
         Route::resource('pages', 'Backend\PageController', ['except' => ['show']]);
         Route::resource('partners', 'Backend\PartnerController', ['except' => ['show']]);
     });
-
 
     Route::group(['middleware' => 'can:' . env('AVUKAT-YÖNETİMİ') . ''], function () {
         Route::resource('attorneys', 'Backend\AttorneyController');
