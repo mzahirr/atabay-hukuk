b071bdc0057d4ac91fc5d5379e0c35ee67e9ffe1
Halil COŞDU <halilcosdu@gmail.com>
2017-07-10 18:38:03 +0300

    fix

--- config/social.php
+++ config/social.php
@@ -10,5 +10,6 @@ return [
     'facebook_app_id'            => '132280227345572',
     'facebook_app_secret'        => '42482c5854400fea4105bc6c3890d207',
     'facebook_page_id'           => '1472442539707618',
-    'facebook_user_access_token' => 'EAAB4TuP2NKQBAPY9d1ZCF9my6nrMrbjvdkoGbfCWLygvw5aXHHqjXufZBdFih6qcs6D9W3GyXFrXIZCRaZBo625iycWeH6cZAiBVkZAHSbGbfdO6rZCxnoRVkn75ZC1xuZC6G1hTHls8djgoPa3FLP7ZAQxeZCRaY1eXYlrjzeKESSuT6URCbC15lOyYulRDkkQxbNWpI8nIpswygZDZD',
+    'facebook_user_access_token' => 'EAAB4TuP2NKQBACV45xXBiPsxaXxWGiRPxQELzUcIRhcnUtBUtTZAKIMBQSRyJVKwEmPxCLKzfGgphoY0FWnX730bvZCZCiBNlU29iDLkD8KGNNA0UlkIS3rTadt2xvdQCtF99PZABzcRxCEh16m3EYf1TWZBueoTnZAx2TOPCDI2FxEXLpcq2D
+',
 ];
